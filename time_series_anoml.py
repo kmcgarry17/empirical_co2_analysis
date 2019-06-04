57bb5c70c0f2443b1eee570be5c3700557d578e5
PMongwe <npmongwe@gmail.com>
2019-06-03 18:02:51 -0600

    Update time_series_anoml.py

--- time_series_anoml.py
+++ time_series_anoml.py
@@ -2,7 +2,8 @@ def F_detrend(var):
     """ computes time series anomaly (remove the trend) for 1D dataset"""
 
     xi = np.arange(np.size(var))
-    slope, intercept, r_value, p_value, std_err = stats.linregress(xi,var)
+    mask = ~np.isnan(var)  ## mask nans
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xi,var[mask])
     line_va = slope*xi+intercept
     var_anoml = var - line_var_x
     
